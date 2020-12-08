818f2a06047d0f65d78b5b4b2d79c5fd0d09d72b
Andres Vallecilla <andresfelipe083195@gmail.com>
2020-12-08 14:46:30 -0500

    install bootstrap and change sass to scss

--- src/app/components/poke-detail/poke-detail.component.ts
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-poke-detail',
   templateUrl: './poke-detail.component.html',
-  styleUrls: ['./poke-detail.component.sass']
+  styleUrls: ['./poke-detail.component.scss']
 })
 export class PokeDetailComponent implements OnInit {
 
--- src/app/components/poke-table/poke-table.component.ts
+++ src/app/components/poke-table/poke-table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-poke-table',
   templateUrl: './poke-table.component.html',
-  styleUrls: ['./poke-table.component.sass']
+  styleUrls: ['./poke-table.component.scss']
 })
 export class PokeTableComponent implements OnInit {
 
