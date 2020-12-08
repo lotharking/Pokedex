bfaf7d563eaaacdcb75352d2ed7456255fe13848
Andres Vallecilla <andresfelipe083195@gmail.com>
2020-12-08 13:53:05 -0500

    components and initializace, error in img pokeheader

---
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-detail',
+  templateUrl: './poke-detail.component.html',
+  styleUrls: ['./poke-detail.component.sass']
+})
+export class PokeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/poke-table/poke-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-table',
+  templateUrl: './poke-table.component.html',
+  styleUrls: ['./poke-table.component.sass']
+})
+export class PokeTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
